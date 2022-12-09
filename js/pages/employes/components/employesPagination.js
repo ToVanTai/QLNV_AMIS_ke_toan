9231607d71dc0af2772f408c3b46cf60ce1d8021
tovantai <tovantaidz2001@gmail.com>
2022-12-09 12:05:39 +0700

    fix ui employesPage, make ui popupnotify

--- js/pages/employes/components/employesPagination.js
+++ js/pages/employes/components/employesPagination.js
@@ -23,7 +23,43 @@ function employesPagination (total, current) {
     if (current < total) {
       liTagHtml += `<li data-index=${current + 1} class="next">Sau</li>`
     }
-  } else if (total > 5) { }
+  } else if (total > 5) {
+    let before = current - 1
+    let after = current + 1
+    if (current > 1) {
+      liTagHtml += `<li data-index=${current - 1} class="prev">Trước</li>`
+    }
+    if (current > 2) {
+      liTagHtml += `<li data-index=1 class="num">1</li>`
+      if (current > 3) {
+        liTagHtml += `<li class="dots">...</li>`
+      }
+    }
+    if (current == 1) {
+      before++
+      after++
+    }
+    if (current == total) {
+      before--
+      after--
+    }
+    for (let i = before; i <= after; i++) {
+      if (i == current) {
+        liTagHtml += `<li class="num active">${i}</li>`
+      } else {
+        liTagHtml += `<li data-index=${i} class="num">${i}</li>`
+      }
+    }
+    if (current < total - 1) {
+      if (current < total - 2) {
+        liTagHtml += `<li class="dots">...</li>`
+      }
+      liTagHtml += `<li data-index=${total} class="num">${total}</li>`
+    }
+    if (current < total) {
+      liTagHtml += `<li data-index=${current + 1} class="next">Sau</li>`
+    }
+  }
   paginationElm.innerHTML = liTagHtml
 
   // thêm sự kiện click vào pagination sau khi rendering pagination
@@ -38,4 +74,4 @@ function employesPagination (total, current) {
   }
 
 }
-employesPagination(5, 1)
+employesPagination(10, 1)
